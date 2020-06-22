cab065c869fe15f5e7b6890722bc4e83e7dc9bf9
Dke <evapromshelko@mail.ru>
2020-06-22 14:26:00 +0300

    Part 3 finished

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,88 +1,61 @@
-const express = require('express');
-const cors = require('cors');
-const app = express();
-require('dotenv').config();
-const Person = require('./models/person');
+const express = require('express')
+const cors = require('cors')
+const app = express()
+require('dotenv').config()
+const Person = require('./models/person')
 
-app.use(cors());
-app.use(express.json());
-app.use(express.static('build'));
+app.use(cors())
+app.use(express.json())
+app.use(express.static('build'))
 
-var morgan = require('morgan');
+var morgan = require('morgan')
 
-morgan.token('req-body', function (req, res) {
-	return JSON.stringify(req.body);
-});
+morgan.token('req-body', function (req) {
+	return JSON.stringify(req.body)
+})
 
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req-body'));
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req-body'))
 
-let persons = [
-	{
-		name: 'Arto Hellas',
-		number: '040-123456',
-		id: 1,
-	},
-	{
-		name: 'Ada Lovelace',
-		number: '39-44-5323523',
-		id: 2,
-	},
-	{
-		name: 'Dan Abramov',
-		number: '12-43-234345',
-		id: 3,
-	},
-	{
-		name: 'Mary Poppendieck',
-		number: '39-23-6423122',
-		id: 4,
-	},
-	{
-		name: 'Alex Rudy',
-		number: '112-864939',
-		id: 5,
-	},
-];
 
 const generateId = () => {
-	const newId = Math.floor(Math.random() * 10000000);
-	return newId;
-};
+	const newId = Math.floor(Math.random() * 10000000)
+	return newId
+}
 
 app.get('/info', (req, res) => {
 	Person.find({}).then((persons) => {
-		const info = `The phonebook currently has ${persons.length} contacts. This request processed at ${new Date()}.`;
+		const info = `The phonebook currently has ${persons.length} contacts. This request processed at ${new Date()}.`
 
-		res.json(info);
-	});
-});
+		res.json(info)
+	})
+})
 
 app.get('/api/persons', (req, res) => {
 	Person.find({}).then((persons) => {
-		console.log(persons);
-		res.json(persons);
-	});
-});
+		console.log(persons)
+		res.json(persons)
+	})
+})
 
 app.get('/api/persons/:id', (req, res, next) => {
 	Person.findById(req.params.id)
 		.then((person) => {
 			if (person) {
-				res.json(person);
+				res.json(person)
 			} else {
-				res.status(404).end();
+				res.status(404).end()
 			}
 		})
-		.catch((error) => next(error));
-});
+		.catch((error) => next(error))
+})
 
 app.post('/api/persons/', (req, res, next) => {
-	const body = req.body;
+	const body = req.body
 
 	if (!body.number || !body.name) {
 		return res.status(400).json({
 			error: 'Important data is missing! Both name and number must be included!',
-		});
+		})
 	}
 
 	const person = new Person({
@@ -90,60 +63,60 @@ app.post('/api/persons/', (req, res, next) => {
 		number: body.number,
 		id: generateId(),
 		date: new Date(),
-	});
+	})
 
 	person
 		.save()
 		.then((savedPerson) => {
-			res.json(savedPerson);
+			res.json(savedPerson)
 		})
-		.catch((error) => next(error));
-});
+		.catch((error) => next(error))
+})
 
 app.delete('/api/persons/:id', (req, res, next) => {
 	Person.findByIdAndRemove(req.params.id)
-		.then((result) => {
-			res.status(204).end();
+		.then(() => {
+			res.status(204).end()
 		})
-		.catch((error) => next(error));
-});
+		.catch((error) => next(error))
+})
 
 app.put('/api/persons/:id', (req, res, next) => {
-	const body = req.body;
+	const body = req.body
 
 	const person = {
 		name: body.name,
 		number: body.number,
-	};
+	}
 
 	Person.findByIdAndUpdate(req.params.id, person, { new: true, runValidators: true })
 		.then((updatedPerson) => {
-			res.json(updatedPerson.toJSON());
+			res.json(updatedPerson.toJSON())
 		})
-		.catch((error) => next(error));
-});
+		.catch((error) => next(error))
+})
 
 const unknownEndpoint = (req, res) => {
-	res.status(404).send({ error: 'unknown endpoint' });
-};
+	res.status(404).send({ error: 'unknown endpoint' })
+}
 
-app.use(unknownEndpoint);
+app.use(unknownEndpoint)
 
 const errorHandler = (error, req, res, next) => {
-	console.error(error.message);
+	console.error(error.message)
 
 	if (error.name === 'CastError') {
-		return res.status(400).send({ error: 'malformatted id' });
+		return res.status(400).send({ error: 'malformatted id' })
 	} else if (error.name === 'ValidationError') {
-		return res.status(400).json({ error: error.message });
+		return res.status(400).json({ error: error.message })
 	}
 
-	next(error);
-};
+	next(error)
+}
 
-app.use(errorHandler);
+app.use(errorHandler)
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT
 app.listen(PORT, () => {
-	console.log(`Phonebook app server is now running on port ${PORT}`);
-});
+	console.log(`Phonebook app server is now running on port ${PORT}`)
+})
